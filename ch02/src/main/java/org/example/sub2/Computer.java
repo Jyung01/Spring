51009b566ada964d10259737007277fc8b6837c5
Jyung01 <wldnd9895@naver.com>
2026-06-15 12:14:11 +0900

    스프링 실습

---
+++ ch02/src/main/java/org/example/sub2/Computer.java
@@ -0,0 +1,36 @@
+package org.example.sub2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("dell")
+public class Computer {
+
+    // 필드 주입 ( 비권장 )
+    @Autowired
+    private CPU cpu;
+
+
+    private RAM ram;
+    private SSD ssd;
+
+    // 생성자 주입
+    @Autowired
+    public Computer(RAM ram) {
+        this.ram = ram;
+    }
+
+    // 세터 주입
+    @Autowired
+    public void setSsd(SSD ssd) {
+        this.ssd = ssd;
+    }
+
+    public void show() {
+        cpu.show();
+        ram.show();
+        ssd.show();
+    }
+
+
+}
